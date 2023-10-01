7921dd3191a3fdab066e31b59bf69cf0c7ffc7bd
André Feitosa <144561856+andrefeitosa9@users.noreply.github.com>
2023-10-01 16:14:24 -0300

    Desafio 022 - Manipulando String

---
+++ Desafio 022.py
@@ -0,0 +1,19 @@
+# Exercício Python 22: Crie um programa que leia o nome completo de uma pessoa e mostre:
+# – O nome com todas as letras maiúsculas e minúsculas.
+# – Quantas letras ao todo (sem considerar espaços).
+# – Quantas letras tem o primeiro nome.
+
+nome_completo = input('Escreva seu nome completo: ')
+
+nome_completo_maiusculo = nome_completo.upper()
+nome_completo_minusculo = nome_completo.lower()
+
+print (f'O nome digitado foi: {nome_completo} \n')
+print (f'Esse nome todo em maiúsculo fica: {nome_completo_maiusculo} \n')
+print (f'Esse nome todo em minúsculo fica: {nome_completo_minusculo} \n')
+
+numero_caracteres = int(len(nome_completo) - int(nome_completo.count(" ")))
+
+print (f' A quantidade de caracteres nesse nome é de : {numero_caracteres}')
+
+
