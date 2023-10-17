b31f86de87fb56751d1eda6fd6a3c209be30ce37
andrefeitosa9 <andrefeitosa9@gmail.com>
2023-10-17 17:43:29 -0300

    Deletando arquivos antigos

--- Desafio 004.py
+++
@@ -1,9 +0,0 @@
-# Desafio 004 - Dissecando uma variável
-
-algo = input('Digite algo: ')
-print (f'Esse algo é um número? {algo.isnumeric()} \n')
-print (f'Esse algo é um texto? {algo.isalpha()} \n')
-print (f'Esse algo é alfanumérico? {algo.isalnum()} \n')
-print (f'Esse algo está maiúsculo? {algo.isupper()} \n')
-print (f'Esse algo está minúsculo? {algo.islower()} \n')
-print (f'Esse algo está capitalizado? {algo.istitle()}')
